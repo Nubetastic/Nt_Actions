9aa8ae9342becbf87f12fabe88d81448895e0dba
Nubetastic <tgandolfo357@gmail.com>
2026-07-30 21:14:27 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 description 'Shared object pose libraries with administrative batch review'
-version '2.0.0'
+version '2.1.0'
 
 dependencies { 'ox_lib', 'ox_target' }
 
